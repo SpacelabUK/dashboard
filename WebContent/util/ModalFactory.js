d8d4435c643790063d0a427b2ba52e05c3cb44e2
Petros Koutsolampros <petros@orange-vertex.net>
2015-08-27 17:44:22 +0100

    Minute: Added more options to matcher modal, added modal to select files

--- WebContent/util/ModalFactory.js
+++ WebContent/util/ModalFactory.js
@@ -1,5 +1,6 @@
 app.factory('ModalFactory', [
-		'$modal', function($modal) {
+		'$modal',
+		function($modal) {
 			var waitModalInstance;
 			var waitData = {
 				text : 'Loading...'
@@ -60,11 +61,79 @@ app.factory('ModalFactory', [
 						}
 					});
 					return promise;
+				},
+				openSelectFileModal : function(title, description, accept, uploader,
+						okAction, fileValid) {
+					var promise = $modal.open({
+						templateUrl : 'util/selectFileModal.html',
+						controller : 'selectFileDialog',
+						resolve : {
+							title : function() {
+								return title;
+							},
+							description : function() {
+								return description;
+							},
+							accept : function() {
+								return accept;
+							},
+							uploader : function() {
+								return uploader;
+							},
+							okAction : function() {
+								return okAction;
+							},
+							fileValid : function() {
+								return fileValid;
+							}
+						}
+					});
+					return promise;
 				}
-			}
+			};
 			return pub;
 		}
 ]);
+
+app.controller('selectFileDialog', [
+		'$scope',
+		'$modalInstance',
+		'FileUploader',
+		'title',
+		'description',
+		'accept',
+		'uploader',
+		'okAction',
+		'fileValid',
+		function($scope, $modalInstance, FileUploader, title, description, accept,
+				uploader, okAction, fileValid) {
+			"use strict";
+			$scope.title = title;
+			$scope.description = description;
+			$scope.accept = accept;
+			if (okAction)
+				$scope.okAction = okAction;
+			else
+				$scope.okAction = "Continue";
+			if (fileValid)
+				$scope.fileValid = fileValid;
+			else
+				$scope.fileValid = function() {
+					return true;
+				};
+			if (uploader)
+				$scope.uploader = uploader;
+			else
+				$scope.uploader = new FileUploader({});
+			$scope.attach = function() {
+				$modalInstance.close($scope.uploader);
+			};
+			$scope.cancel = function() {
+				$modalInstance.dismiss('cancel');
+			};
+		}
+]);
+
 app.controller('waitModalCtrl', [
 		'$scope', '$modalInstance', 'waitData',
 		function($scope, $modalInstance, waitData) {
@@ -108,6 +177,17 @@ app.factory('MatcherFactory', [
 			return pub;
 		}
 ]);
+/**
+ * @param options.prematch:
+ *          Will try to match FROM and TO elements. If set to 'ignorecase' it
+ *          will treat the values as strings and compare ignoring case. To
+ *          define alternative properties to compare user fromPrematch and
+ *          toPrematch
+ * @param options.fromPrematch:
+ *          see options.prematch
+ * @param options.toPrematch:
+ *          see options.prematch
+ */
 app
 		.controller(
 				'matcherModalInstance',
@@ -121,6 +201,7 @@ app
 						'options',
 						function($scope, $modalInstance, type, types, fromElements,
 								toElements, options) {
+							var i, j;
 							$scope.type = type;
 							$scope.types = types;
 							$scope.inFilePredicate = 'alias';
@@ -129,36 +210,60 @@ app
 							$scope.options = options;
 							$scope.attach = function() {
 								$modalInstance.close(fromElements);
-							}
+							};
 							if (toElements)
 								$scope.toElements = toElements;
 							else
 								$scope.toElements = [];
-							for (var i = 0; i < toElements.length; i++)
+							for (i = 0; i < toElements.length; i++)
 								toElements[i].available = true;
-							for (var i = 0; i < fromElements.length; i++)
-								if (fromElements[i].prematch) {
-									if (fromElements[i].prematchproperty) {
-										for (var j = 0; j < toElements.length; j++)
-											if (toElements[j][fromElements[i].prematchproperty] == fromElements[i].prematch) {
-												fromElements[i].match = toElements[j];
-												toElements[j].available = false;
-												break;
-											}
-									}
-									for (var j = 0; j < toElements.length; j++)
-										if (toElements[j].alias == fromElements[i].prematch) {
+
+							if (options.prematch)
+								for (i = 0; i < fromElements.length; i++) {
+									var fromPrematch, toPrematch;
+									if (options.fromPrematch)
+										fromPrematch = options.fromPrematch;
+									else
+										fromPrematch = "alias";
+									if (options.toPrematch)
+										toPrematch = options.toPrematch;
+									else
+										toPrematch = "alias";
+									for (j = 0; j < toElements.length; j++)
+										if ((options.prematch === 'ignorecase' && eql(
+												toElements[j][toPrematch],
+												fromElements[i][fromPrematch])) ||
+												toElements[j][toPrematch] === fromElements[i][fromPrematch]) {
 											fromElements[i].match = toElements[j];
 											toElements[j].available = false;
 											break;
 										}
 								}
+							else {
+								for (i = 0; i < fromElements.length; i++)
+									if (fromElements[i].prematch) {
+										if (fromElements[i].prematchproperty) {
+											for (j = 0; j < toElements.length; j++)
+												if (toElements[j][fromElements[i].prematchproperty] == fromElements[i].prematch) {
+													fromElements[i].match = toElements[j];
+													toElements[j].available = false;
+													break;
+												}
+										}
+										for (j = 0; j < toElements.length; j++)
+											if (toElements[j].alias == fromElements[i].prematch) {
+												fromElements[i].match = toElements[j];
+												toElements[j].available = false;
+												break;
+											}
+									}
+							}
 							$scope.cancel = function() {
 								$modalInstance.dismiss('cancel');
 							};
 							$scope.getElementProperty = function(element, property) {
 								return element[property];
-							}
+							};
 							$scope.selectFrom = function(element) {
 								for (var i = 0; i < fromElements.length; i++) {
 									fromElements[i].selected = false;
@@ -166,14 +271,14 @@ app
 										fromElements[i].match.available = false;
 								}
 								element.selected = true;
-								for (var i = 0; i < toElements.length; i++)
+								for (i = 0; i < toElements.length; i++)
 									toElements[i].selected = false;
 								if (element.match) {
 									element.match.selected = true;
 									element.match.available = true;
 								}
 
-							}
+							};
 							$scope.selectTo = function(element) {
 								if (!element.available)
 									return;
@@ -189,14 +294,14 @@ app
 									selectedFrom.match = null;
 									return;
 								}
-								for (var i = 0; i < toElements.length; i++)
+								for (i = 0; i < toElements.length; i++)
 									toElements[i].selected = false;
 								if (selectedFrom) {
 									element.selected = true;
 									selectedFrom.match = element;
 								}
 
-							}
+							};
 							$scope.deselect = function() {
 								for (var i = 0; i < fromElements.length; i++) {
 									if (fromElements[i].selected) {
@@ -206,7 +311,7 @@ app
 										break;
 									}
 								}
-							}
+							};
 							$scope.selectedElementIsNew = function() {
 								for (var i = 0; i < fromElements.length; i++) {
 									if (fromElements[i].selected) {
@@ -214,14 +319,14 @@ app
 										break;
 									}
 								}
-							}
+							};
 							$scope.elementIsNew = function(element) {
 								if (element.match) {
 									element.match.selected = false;
 									element.match.available = true;
 								}
 								element.match = '*';
-							}
+							};
 							$scope.selectedElementIgnore = function() {
 								for (var i = 0; i < fromElements.length; i++) {
 									if (fromElements[i].selected) {
@@ -229,42 +334,56 @@ app
 										break;
 									}
 								}
-							}
+							};
 							$scope.elementIgnore = function(element) {
 								if (element.match) {
 									element.match.selected = false;
 									element.match.available = true;
 								}
 								element.match = '-';
-							}
+							};
 							$scope.reset = function() {
-								for (var i = 0; i < fromElements.length; i++) {
+								for (i = 0; i < fromElements.length; i++) {
 									fromElements[i].selected = false;
 									fromElements[i].match = null;
 								}
-								for (var i = 0; i < toElements.length; i++) {
+								for (i = 0; i < toElements.length; i++) {
 									toElements[i].selected = false;
 									toElements[i].available = true;
 								}
-							}
+							};
 							$scope.getElementState = function(element) {
 								if (!element.available)
 									return 'disabled';
 								if (element.selected)
 									return 'highlight';
-							}
+							};
 							$scope.getElementMatch = function(element) {
 								if (element.match && element.match.alias)
 									return element.match.alias;
-								if (element.match == '*')
+								if (element.match && element.match.manualMatch)
+									return element.match[element.match.manualMatch] +
+											" (user set)";
+								if (element.match === '*')
 									return '- NEW -';
-								if (element.match == '-')
+								if (element.match === '-')
 									return '- IGNORE -';
 
-							}
+							};
 							$scope.capitalize = function(type) {
 								return type.charAt(0).toUpperCase() + type.substring(1);
-							}
+							};
+							$scope.setValueManually = function(element) {
+								if ($scope.options && $scope.options.toMatchProperty) {
+									var prop = prompt($scope.options.toMatchProperty.prompt, "");
+									if (prop) {
+										element.match = {
+											manualMatch : $scope.options.toMatchProperty.name
+										};
+										element.match[$scope.options.toMatchProperty.name] = prop;
+									}
+								}
+							};
 						}
 				]);
 app.controller('confirmDialog', [
