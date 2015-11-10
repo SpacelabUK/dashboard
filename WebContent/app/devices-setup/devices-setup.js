95776468ebdd493853d9bb66d410b16e3cbe92ea
Dipesh Patel <dipesh.patel@spacelab.co.uk>
2015-11-10 11:18:08 +0000

    Just added some basic modal wait pages.

--- WebContent/app/devices-setup/devices-setup.js
+++ WebContent/app/devices-setup/devices-setup.js
@@ -1,9 +1,9 @@
 (function () {
     "use strict";
     angular.module('app.devices-setup').controller('DevicesSetupController', DevicesSetupController);
-    DevicesSetupController.$inject = ['$scope', '$stateParams','HTTPFactory', 'FileUploader'];
+    DevicesSetupController.$inject = ['modalFactory', '$stateParams','HTTPFactory', 'FileUploader'];
 
-    function DevicesSetupController($scope, $stateParams, HTTPFactory, FileUploader) {
+    function DevicesSetupController(modalFactory, $stateParams, HTTPFactory, FileUploader) {
         var ob = this;
 
         var study_id = $stateParams['study_id'];
@@ -32,43 +32,54 @@
         };
 
         ob.uploader.onSuccessItem = function(item, response, status, headers) {
+            modalFactory.modifyWaitMessage("Generating files for " + ob.selectedId);
             HTTPFactory.propulsionPost('/studies/' + study_id + '/device/' + ob.selectedId + '/package').then(
                 function(){
                     HTTPFactory.propulsionGet("/studies/" + study_id +"/device/plans").then(
                         function(response){
+                            modalFactory.closeWaitModal();
                             ob.devicePlans = response.data
                         }, function (error) {
-                            console.log(error);
+                            modalFactory.closeWaitModal();
+                            modalFactory.openErrorModal(error);
                         }
                     );
                 }, function (error) {
-                    console.log(error);
+                    modalFactory.closeWaitModal();
+                    modalFactory.openErrorModal(error);
                 }
             );
         };
 
         ob.attach = function(){
+            modalFactory.openWaitModal('Uploading file.');
             ob.uploader.uploadAll();
         };
 
         ob.deleteDevicePlan = function(devicePlan) {
+            modalFactory.openWaitModal('Deleting Device data for ' + devicePlan.device_name);
             HTTPFactory.propulsionDelete('/studies/' + study_id + '/device/' + devicePlan.device_name + '/package').then(
                 function(){
+                    modalFactory.modifyWaitMessage("Deleting Device plans.");
                     HTTPFactory.propulsionDelete("/device/plans/" + devicePlan.id).then(
                         function(){
                             HTTPFactory.propulsionGet("/studies/" + study_id +"/device/plans").then(
                                 function(response){
+                                    modalFactory.closeWaitModal();
                                     ob.devicePlans = response.data
                                 }, function (error) {
-                                    console.log(error);
+                                    modalFactory.closeWaitModal();
+                                    modalFactory.openErrorModal(error);
                                 }
                             );
                         }, function (error) {
-                            console.log(error);
+                            modalFactory.closeWaitModal();
+                            modalFactory.openErrorModal(error);
                         }
                     );
                 }, function (error) {
-                    console.log(error);
+                    modalFactory.closeWaitModal();
+                    modalFactory.openErrorModal(error);
                 }
             );
         }
