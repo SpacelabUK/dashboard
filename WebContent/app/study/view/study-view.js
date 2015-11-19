f41ba81240b019cfb7ce4d954fc1d93986fdc3de
Dipesh Patel <dipesh.patel@spacelab.co.uk>
2015-11-19 16:59:02 +0000

    Use propulsion to fetch study details.

--- WebContent/app/study/view/study-view.js
+++ WebContent/app/study/view/study-view.js
@@ -1,20 +1,37 @@
 (function () {
     "use strict";
-    angular.module('app.study.view').controller('ViewStudyController',
-        ViewStudyController);
-    ViewStudyController.$inject = [
-        '$stateParams', 'dataService', '$modal'
-    ];
-    function ViewStudyController($stateParams, dataService, $modal) {
+    angular.module('app.study.view').controller('ViewStudyController', ViewStudyController);
+    ViewStudyController.$inject = ['$stateParams', 'HTTPFactory', '$modal'];
+
+    function ViewStudyController($stateParams, HTTPFactory, $modal) {
         var vm = this;
         vm.id = $stateParams.studyid;
         vm.spacePredicate = "alias";
+        vm.counts = {};
 
-        dataService.getOccupancyPerSpaceAndRound(vm.id).then(function (response) {
-            vm.spaces = response.data;
-        }, function (error) {
+        HTTPFactory.propulsionGet('/studies/' + vm.id + '/spaces').then(function(response) {
+            vm.spaces = response.data.content;
+            HTTPFactory.propulsionGet('/studies/'+vm.id+'/observation/rounds?sort=day&sort=round').then(function(response) {
+                vm.rounds = response.data;
+                vm.spaces.forEach(function(space){
+                    vm.counts[space.id] = {};
+                    vm.rounds.forEach(function(round){
+                        HTTPFactory.propulsionGet("/spaces/" + space.id + "/observation/rounds/" + round.id + "/items/count").then(
+                            function(response){
+                                vm.counts[space.id][round.id] = response.data.observation_items_count;
+                            }, function(error){
+                                console.log(error)
+                            }
+                        );
+                    });
+                });
+            }, function(error){
+                console.log(error);
+            });
+        }, function (error){
             console.log(error);
         });
+
         vm.getFuncs = function (space) {
             dataService.getSpaceFunctionPolygons(space.id).then(function (response) {
                 var promise = $modal.open({
@@ -59,14 +76,14 @@
                     console.log(error);
                 });
         };
-        vm.getSnapshotColour = function (snapshot) {
-            return 'rgb(255,' + (255 - ((snapshot.occupancy * 128) | 0)) + ',' +
-                (200 - ((snapshot.occupancy * 200) | 0)) + ")";
+        vm.calculateColour = function (spaceId, roundId) {
+            return 'rgb(255,' + (255 - ((vm.counts[spaceId][roundId] * 128) | 0)) + ',' +
+                (200 - ((vm.counts[spaceId][roundId] * 200) | 0)) + ")";
         };
         vm.empty = function () {
 
         };
-        vm.showSnapshot = function (space, snapshot) {
+        vm.showObservationRound = function (space, snapshot) {
             dataService.getSnapshotData(snapshot.snapshot_id).then(
                 function (response) {
                     var promise = $modal.open({
